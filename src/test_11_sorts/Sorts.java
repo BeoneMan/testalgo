00c2164c09f684313c998eb29992e7f46d6f9c7f
driveman <driveman@qq.com>
2019-08-14 11:32:30 +0800

    完成添加Sorts，插入排序和选择排序

--- src/test_11_sorts/Sorts.java
+++ src/test_11_sorts/Sorts.java
@@ -27,6 +27,8 @@ public class Sorts {
         }
     }
 
+
+
     //优化冒泡排序
     public static void bubbleSort2(int[] a ,int n){
 
@@ -55,14 +57,79 @@ public class Sorts {
         }
     }
 
+    //左神冒泡排序
+    public static void bubbleSort3(int[]a,int n){
+        if(n<=0)return;
+        for(int i=n-1;n>0;n--){
+            boolean flag=false;
+            for(int j=0;j<i;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=true;
+                }
+            }
+            if(!flag)break;
+        }
+    }
+
+    //插入排序
+    public static void insertSort(int[] a,int n){
+        if(n<=0)return;
+        for(int i=1;i<n;i++){
+           int value=a[i];
+           int j=i-1;
+           for(;j>=0;j--){
+               if(a[j]>value){
+                   a[j+1]=a[j];
+               }else{
+                   break;
+               }
+           }
+           a[j+1]=value;
+        }
+    }
+
+    //选择排序
+    public static void selectionSort(int[] a,int n){
+        if(n<=0)return;
+        for(int i=0;i<n-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<n-1;j++){
+                if(a[j]>a[j+1]){
+                    minIndex=j+1;
+                }
+            }
+            int temp=a[i];
+            a[i]=a[minIndex];
+            a[minIndex]=temp;
+        }
+    }
+
+
     public static void main(String[] args) {
         //冒泡排序测试
         /*int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
         bubbleSort(array, array.length);
         System.out.println(Arrays.toString(array));*/
         //优化后冒泡排序测试
-        int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        /*int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
         bubbleSort2(array, array.length);
+        System.out.println(Arrays.toString(array)); */
+        //左神冒泡排序
+       /* int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        bubbleSort3(array, array.length);
+        System.out.println(Arrays.toString(array));*/
+
+        //插入排序
+        /*int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        insertSort(array, array.length);
+        System.out.println(Arrays.toString(array));  */
+
+        //选择排序
+        int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        selectionSort(array, array.length);
         System.out.println(Arrays.toString(array));
     }
 }
