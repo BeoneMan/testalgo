20454227487ec5ab5febdd0ebab52849138d39c4
beoneman <Qjy19940708>
2019-12-17 17:28:16 +0800

    完成手写单链表

--- src/test_06_linkedlist/test01/SinglyLinkedList.java
+++ src/test_06_linkedlist/test01/SinglyLinkedList.java
@@ -13,12 +13,13 @@ public class SinglyLinkedList {
     //通过value寻找并返回Node
     public Node findByValue(int value) {
         if (head == null) return null;
-        Node next = head.next;
-        while (next != null && next.data != value) {
-            next = next.next;
+        if (value == head.data) return head;
+        Node p = head;
+        while (p != null && p.data != value) {
+            p = p.next;
         }
-        if (next == null) return null;
-        return next;
+        if (p == null) return null;
+        return p;
     }
 
     //通过index寻找并返回Node
@@ -59,42 +60,70 @@ public class SinglyLinkedList {
             while (q.next != null) {
                 q = q.next;
             }
-            newNode.next=q.next;
+            newNode.next = q.next;
             q.next = newNode;
         }
 
     }
 
     //在哪个节点前插入
-    public void insertBefore(Node node ,int value){
-        if(head==null||node==null)return;
-        if(node==head)insertHead(node);
-        Node newNode=new Node(value,null);
-        Node p =head;
-        while(p!=null&&p.next!=node){
-            p=p.next;
+    public void insertBefore(Node node, int value) {
+        if (head == null || node == null) return;
+        Node newNode = new Node(value, null);
+        Node p = head;
+        if (node == head) {
+            insertHead(newNode);
+            return;
         }
-        if(p==null)return;
-        newNode.next=node;
-        p.next=newNode;
+        while (p != null && p.next != node) {
+            p = p.next;
+        }
+        if (p == null) return;
+        newNode.next = node;
+        p.next = newNode;
     }
 
     //在哪个节点后插入
-public void insertAfter(Node node,int value){
-        if(head==null||node==null)return;
-        Node p =head;
-        Node newNode=new Node(value,null);
-        while(p!=null&&p.next!=node){
-            p=p.next;
+    public void insertAfter(Node node, int value) {
+        if (head == null || node == null) return;
+        Node p = head;
+        Node newNode = new Node(value, null);
+        while (p != null && p != node) {
+            p = p.next;
         }
-        if(p==null)return;
-        newNode.next=p.next.next;
-        p.next.next=newNode;
+        if (p == null) return;
+        newNode.next = p.next;
+        p.next = newNode;
 
-}
+    }
+
     //通过value删除Node节点
-    //通过index删除Node节点
+    public void deleteByValue(int value) {
+        Node deleNode = findByValue(value);
+        if (deleNode == null) return;
+        if (head == deleNode) {
+            head = head.next;
+            return;
+        }
+        Node pre = head;
+        while (pre != null && pre.next != deleNode) {
+            pre = pre.next;
+        }
+        pre.next = deleNode.next;
+    }
 
+    //通过index删除Node节点
+    public void deleteByIndex(int index) {
+        if (index == 0) head = head.next;
+        Node pre = head;
+        int bos = 0;
+        while (pre != null && bos != index - 1) {
+            pre = pre.next;
+            bos++;
+        }
+        if (pre == null) return;
+        pre.next = pre.next.next;
+    }
 
     public static class Node {
         private int data;
@@ -111,5 +140,49 @@ public void insertAfter(Node node,int value){
         public int getData() {
             return data;
         }
+    }
+
+    public void printAll() {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertTail(2);
+//        list.printAll();
+
+        list.insertTail(new Node(3, null));
+//        list.printAll();
+        list.insertHead(10);
+//        list.printAll();
+        list.insertHead(new Node(5, null));
+        list.printAll();
+        Node byValue = list.findByValue(10);
+        System.out.println("寻找的值为：" + byValue.data);
+
+        Node byIndex = list.findByIndex(0);
+        System.out.println("根据索引寻找的值为" + byIndex.data);
+
+        list.insertBefore(byIndex, 1);
+        list.printAll();
+        Node index2 = list.findByIndex(4);
+        list.insertAfter(index2, 90);
+        list.printAll();
+
+        list.deleteByValue(5);
+        list.deleteByValue(90);
+        list.deleteByValue(1);
+        list.printAll();
+
+        list.deleteByIndex(0);
+        list.deleteByIndex(1);
+        list.printAll();
+
     }
 }
